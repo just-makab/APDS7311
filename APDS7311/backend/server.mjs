6efaf2874460ac359edc442a246c340c2991938f
just-makab <lwethusibisi@gmail.com>
2024-07-29 12:10:04 +0200

    More updates

--- APDS7311/backend/server.mjs
+++ APDS7311/backend/server.mjs
@@ -1,1 +1,16 @@
-console.log("Why Why WHYYYYYY")
+import express from "express";
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+
+app.get('/',(req, res)=>{
+    res.send("Whats up buddy")
+})
+
+app.get('/test',(req, res)=>{
+res.send('Testing the /test thing')
+})
+
+
+app.listen(PORT);
